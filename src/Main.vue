99667b5fab0502b2a4d9dd353f1e721e57661f00
Muhammet Bilgin <muhammetbilqin@hotmail.com>
2021-07-14 16:29:47 +0300

    Main and Home update

--- src/Main.vue
+++ src/Main.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="content">
     <HeaderComponent />
-    <TrendsTvsComponent />
+    <router-view />
     <FooterComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/shared/Header.vue";
-import TrendsTvsComponent from "./components/shared/TrendsTvs.vue";
 import FooterComponent from "./components/shared/Footer.vue";
 
 import {ref} from 'vue'
 export default {
-     components: { HeaderComponent,TrendsTvsComponent ,FooterComponent },
+     components: { HeaderComponent,FooterComponent },
     setup() {
 
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,46 +1,203 @@
 <template>
-  
+<section class="trends-tv">
+	<div class="trends-tv__title">Movies & TV</div>
+	<div class="trends-tv__cards-episode">
+		<div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+        <div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+        <div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+        <div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+        <div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+        <div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+		<div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+		<div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+		<div class="trends-tv__card">
+			<div class="trends-tv__top">
+				<img src="../../assets/img/bliss.png" alt="Film Image" class="trends-tv__image">
+			</div>
+			<div class="trends-tv__bottom">
+				<div class="trends-tv__name">
+                    BLİSS
+				</div>
+				<div class="trends-tv__rating">
+					<img src="../../assets/svg/star.svg" alt="Raiting" > 7.0
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+
+</section>
+
 </template>
-
 <script>
-import { onMounted, onUnmounted } from "vue";
+import {ref  } from "vue";
 export default {
+	
+ 
   setup() {
-    onMounted(() => console.log("Mount: ⛰ Home Component"));
-    onUnmounted(() => console.log("unMount: 👋🏻 Home Component "));
+	let movies= ref({});
 
-    return {};
+ 
+	
+
+    return {
+		movies,
+	
+
+    };
+    
   },
+  
+   	
+  
 };
-
-
-
-
-
 </script>
 
 <style lang="scss">
-.home {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: flex-start;
 
-  &__header {
-    flex-grow: 1;
-
-    margin: 0;
-    padding: 20px;
-
-    min-height: 120px;
-
+.trends-tv{
     display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    justify-content: flex-start;
+    flex-direction: column;
+    align-items: center;
+    &__title{
+        @include font-size(18);
+        color:rgb(255, 255, 255);
+    }
+    &__cards-episode{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+		
+    
+        
+        @include mq("mobile", max) {
+        margin: 2px;
+        width: 10px;
+       
+        
 
-    background-color: $blue;
-    color: $white;
-  }
+			
+		}
+    }
+	&__card{
+	
+		background-color: #FFCB14;
+		margin: 20px;
+		font-weight: 600;
+		@include font-size(14);
+	}
+    &__bottom{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 10px;
+    }
 }
+
 </style>
